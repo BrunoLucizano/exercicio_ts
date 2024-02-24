023e084a530e87f37bf4ab594e2efa84b06f4238
BrunoLucizano <brunolucizano@gmail.com>
2024-02-24 13:34:25 -0300

    commit inicial

---
+++ src/funcao.js
@@ -0,0 +1,8 @@
+function multiplicacao(numero1, numero2) {
+    return numero1 * numero2;
+}
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+console.log(multiplicacao(2, 3));
+console.log(saudacao("João"));
